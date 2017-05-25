ac4bde74b299be704df309be6f4a96b408b56fd7
Zulu <jzuluaga55@gmail.com>
2017-05-25 18:26:03 -0500

    Lab 04 Finish

---
+++ AndroidApp/AndroidDialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using PCLProject;
+
+namespace AndroidApp
+{
+    public class AndroidDialog : IDialog
+    {
+        Context context;
+
+        public AndroidDialog(Context context)
+        {
+            this.context = context;
+        }
+
+        public void Show(string message)
+        {
+            var builder = new AlertDialog.Builder(context);
+            var alert = builder.Create();
+            alert.SetTitle("Resultado de la verificación");
+            alert.SetIcon(Resource.Drawable.Icon);
+            alert.SetMessage(message);
+            alert.SetButton("Ok", (s, ev) => { });
+            alert.Show();
+        }
+    }
+}
---
+++ PCLProject/AppValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCLProject
+{
+    public class AppValidator
+    {
+        IDialog dialog;
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Device { get; set; }
+
+        public AppValidator(IDialog dialog)
+        {
+            this.dialog = dialog;
+        }
+
+        public async void Validate()
+        {
+            var serviceClient = new SALLab04.ServiceClient();
+            var svcResult = await serviceClient.ValidateAsync(Email, Password, Device);
+            var result = $"{ svcResult.Status}\n{ svcResult.Fullname}\n{svcResult.Token}";
+            dialog.Show(result);
+        }
+    }
+}
